6b991e3ca1259a40056449f3d59f5fb7f4e02d66
matej <matej.basta@email.cz>
2022-03-08 10:45:44 +0100

    random facts generated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import RandomCat from './components/RandomCat';
+import React, {useEffect, useState} from "react";
 
 function App() {
+
+  const [fact, setFact] = useState("");
+
+  const url = "https://cat-fact.herokuapp.com/facts/random";
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    data && setFact(data.text);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RandomCat fact={fact}/>
   );
 }
 
---
+++ src/components/RandomCat.jsx
@@ -0,0 +1,7 @@
+function RandomCat({fact}) {
+
+    return <h1>{fact}</h1>
+
+}
+
+export default RandomCat;
